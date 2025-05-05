c929ba020826b5b28472db99761392ad85a99d34
Nadhmi JAZI <nadhmi.jazi@gmail.com>
2025-05-05 15:21:48 +0200

    feat: make durability Transient local
    Signed-off-by: Nadhmi JAZI <nadhmi.jazi@gmail.com>

--- src/common.rs
+++ src/common.rs
@@ -52,7 +52,7 @@ pub fn create_qos(depth: usize) -> Profile {
         depth,
         history: HistoryPolicy::KeepLast,
         reliability: ReliabilityPolicy::Reliable,
-        durability: DurabilityPolicy::Volatile,
+        durability: DurabilityPolicy::TransientLocal,
         ..Profile::default()
     }
 }
